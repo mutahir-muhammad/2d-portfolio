d1b02eafe21debd21a9518fcdb8bf7f8665d54f7
Mutahir <mutahir.khan44@yahoo.com>
2024-05-09 00:03:35 +0500

    first commit

---
+++ src/constants.js
@@ -0,0 +1,22 @@
+export const scaleFactor = 3;
+
+export const dialogueData = {
+    pc: `This is my setup. I work mostly in JavaScript these days.
+          I've made a couple of projects in that language. I also like C++ and Python. Anyway regardless of the language, I just like programming.
+          Here is my <a href="https://github.com/mutahir-muhammad" target="_blank">Github</a>!`,
+    "cs-degree": `This is my SE degree. It is still in progress. I'm currently in my 2nd year. I've learned a lot of things in this degree.`,
+    "sofa-table": `That's my sofa. I like to relax here and watch YouTube, listen to music and chill. 
+        I also post on Instagram. Go check <a href="https://www.instagram.com/alphaberose?igsh=MW82ZmQzaGhpZzY3OQ==" target="_blank">my page</a>! (If you like the content) and feel free to connect with me on my <a href="https://www.linkedin.com/in/muhammad-mutahir-160067266/" target="_blank">LinkedIn</a>! `,
+    tv: `That's my screen. I've been watching tech youtubers a lot recently like :
+         <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="www.youtube.com/@Fireship" target="_blank">Fireship</a>,
+        <a href="www.youtube.com/@NeetCode" target="_blank">NeetCode</a> and (sometimes) <a href="www.youtube.com/@AniaKubow" target="_blank">Ania Kubow</a>!`,
+    bed: `This where I sleep. Great ideas come when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.
+    I note my ideas in my notebook so that I can work on them later.`,
+    resume: `This is my desk and on it is my resume. <a href="https://github.com/mutahir-muhammad/resume/blob/main/Muhammad's%20Resume%20(4).pdf" target="_blank">Check it out!!</a>
+        Contact me at mutahir328@gmail.com if you have any interesting job opportunities or if you want to collaborate on a project!`,
+    projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+        I have made many more projects that are present on my github. Check them out!`,
+    library: `There are a lot of  books on my shelves. I own a lot of fiction novels that I still haven't read.
+        Who else compulsively buys books without ever finishing them? I consider buying books as a hobby in itself.`,
+    exit: `If you want to exit my game, just close the tab. ; ) `,
+};
